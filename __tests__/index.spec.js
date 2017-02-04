ddf3849bbe71f2baa8d2ba5b0f4508515e8968e4
bobwei <bob61016@gmail.com>
2017-02-05 01:23:07 +0800

    mv http client setup to beforeAll

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -5,8 +5,27 @@ import axios from 'axios';
 jest.useFakeTimers();
 
 beforeAll(() => {
+  /* setup env */
   const dotenv = require('dotenv');
   dotenv.config({ path: path.join(__dirname, './test.env') });
+
+  /* setup http client */
+  const {
+    PARSE_SERVER_APPLICATION_ID,
+    PARSE_SERVER_JAVASCRIPT_KEY,
+    PARSE_SERVER_URL,
+  } = process.env;
+  axios.defaults.baseURL = PARSE_SERVER_URL;
+  axios.interceptors.request.use(config => ({
+    ...config,
+    headers: {
+      'X-Parse-Application-Id': PARSE_SERVER_APPLICATION_ID,
+      'X-Parse-Javascript-Key': PARSE_SERVER_JAVASCRIPT_KEY,
+      ...config.headers,
+    },
+  }));
+
+  /* setup server */
   const { server } = require('server/server');
   server.setTimeout(3000);
 });
@@ -20,18 +39,8 @@ afterAll(() => {
 });
 
 it('can start server', () => {
-  const {
-    PARSE_SERVER_APPLICATION_ID,
-    PARSE_SERVER_JAVASCRIPT_KEY,
-    PARSE_SERVER_URL,
-  } = process.env;
   return axios
-    .get(`${PARSE_SERVER_URL}/health`, {
-      headers: {
-        'X-Parse-Application-Id': PARSE_SERVER_APPLICATION_ID,
-        'X-Parse-Javascript-Key': PARSE_SERVER_JAVASCRIPT_KEY,
-      },
-    })
+    .get('/health')
     .then((res) => {
       expect(res.status).toBe(200);
     });
